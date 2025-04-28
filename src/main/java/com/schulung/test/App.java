bc3f3ec7947665291e2fb16a06458c78b5759fef
Elias Prohaszka <eliasprohaszka@gmail.com>
2025-04-28 19:49:15 +0200

    update

--- src/main/java/com/schulung/test/App.java
+++ src/main/java/com/schulung/test/App.java
@@ -1,6 +1,10 @@
 package com.schulung.test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Hello world!
@@ -8,6 +12,42 @@ import java.util.ArrayList;
  */
 public class App {
     public static void main(String[] args) {
+
+
+        TreeSet<String> städte = new TreeSet<>();
+        städte.add("Berlin");
+        städte.add("Hamburg");
+        städte.add("München");
+
+        // Einfache Iteration
+        for (String stadt : städte) {
+            System.out.println(stadt);
+        }
+
+        TreeSet<Integer> zahlen = new TreeSet<>();
+        zahlen.add(10);
+        zahlen.add(5);
+        zahlen.add(20);
+
+        Iterator<Integer> iterator = zahlen.iterator();
+        while (iterator.hasNext()) {
+            Integer zahl = iterator.next();
+            System.out.println(zahl);
+            // Mit iterator.remove() könnte man Elemente während der Iteration entfernen
+        }
+
+        TreeMap<String, Integer> personenAlter = new TreeMap<>();
+        personenAlter.put("Hans", 25);
+        personenAlter.put("Anna", 30);
+        personenAlter.put("Lisa", 22);
+
+        // Iteration über alle Einträge
+        for (Map.Entry<String, Integer> eintrag : personenAlter.entrySet()) {
+            System.out.println(eintrag.getKey() + " ist " + eintrag.getValue() + " Jahre alt");
+        }
+
+
+
         // ----------- ARRAY BEISPIEL -----------
         System.out.println("🔢 Array Beispiel:");
 
@@ -15,6 +55,11 @@ public class App {
         for (int i = 0; i < zahlenArray.length; i++) {
             System.out.println("Index " + i + ": " + zahlenArray[i]);
         }
+
+        int[][] zahlenArray2 = {{ 10, 11, 12, 13, 14 },
+                                { 20, 21, 22, 4, 5 }};
+
+        System.out.println(zahlenArray2[0][2]);
 
         // ----------- ARRAYLIST BEISPIEL -----------
         System.out.println("\n📦 ArrayList Beispiel:");
